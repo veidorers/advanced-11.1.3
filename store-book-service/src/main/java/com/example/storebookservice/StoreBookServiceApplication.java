b9eceaff81bf68a6327bee5daffaaadd01d9dffe
Sergey Fomenko <artem.work749@gmail.com>
2024-03-14 07:51:48 +0300

    Add Spring Boot Actuator (get config server updates by manual request to the client's actuator

--- store-book-service/src/main/java/com/example/storebookservice/StoreBookServiceApplication.java
+++ store-book-service/src/main/java/com/example/storebookservice/StoreBookServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.storebookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 public class StoreBookServiceApplication {
